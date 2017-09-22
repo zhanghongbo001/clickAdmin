be1d58b252dc0154497e89c2fe28f81cb6fc98ef
zhanghongbo <1002688187@qq.com>
2017-09-22 09:38:30 +0800

    修改说明1

--- src/main/webapp/WEB-INF/view/uploader/uploader.jsp
+++ src/main/webapp/WEB-INF/view/uploader/uploader.jsp
@@ -1,9 +1,60 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Title</title>
-</head>
-<body>
-<h2>上传组件</h2>
+<%@page pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html lang="zh_CN">
+<%@include file="/WEB-INF/view/layout/header.jsp" %>
+<!-- laypage css-->
+<link rel="stylesheet" href="${ctx}/assets/common/lib/laypage/skin/laypage.css">
+<link rel="stylesheet" href="${ctx}/assets/common/webuploader-0.1.5/webuploader.css">
+<!--引入JS-->
+<script type="text/javascript" src="${ctx}/assets/common/webuploader-0.1.5/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="${ctx}/assets/common/webuploader-0.1.5/webuploader.js"></script>
+<body class="fixed-left">
+<div id="wrapper">
+    <%@include file="/WEB-INF/view/layout/top.jsp" %>
+    <%@include file="/WEB-INF/view/layout/left.jsp" %>
+    <div class="content-page">
+        <div class="content">
+            <div class="container">
+                <!-- Page-Title -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4 class="pull-left page-title">web uploader上传文件</h4>
+                    </div>
+                </div>
+                <div  style="width: 400px;height: 400px;">
+                    <div id="post-container" class="container">
+                        <div class="col-md-9">
+                            <div class="page-container">
+                                <div id="uploader" class="wu-example">
+                                    <div id="thelist" class="uploader-list"></div>
+                                    <div class="btns">
+                                        <div id="picker">选择文件</div>
+                                        <button id="ctlBtn" class="btn btn-default">开始上传</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer text-right">
+        2016 © Moltran.
+    </footer>
+</div>
+</div>
+<%@include file="/WEB-INF/view/layout/footer.jsp" %>
+<!--laypage js-->
+<script src="${ctx}/assets/common/lib/laypage/laypage.js"></script>
+<script>
+    window._TOTAL_PAGE = '${adminlist.getPages()}';
+    window._CURRENT_PAGE = '${pageNum}';
+</script>
+<!-- 其他功能JS -->
+<script src="${ctx}/assets/uploader/uploader.js"></script>
+
+<%--<script src="${ctx}/assets/common/js/page.js"></script>--%>
 </body>
 </html>
---
+++ src/main/webapp/assets/uploader/uploader.js
@@ -0,0 +1,112 @@
+// 文件上传
+jQuery(function() {
+    var $ = jQuery,
+        $list = $('#thelist'),
+        $btn = $('#ctlBtn'),
+        state = 'pending',
+        uploader;
+    uploader = WebUploader.create({
+        // 不压缩image
+        resize: false,
+        // swf文件路径，需要用到flash的时候BASE_URL自己根据需要定义 也可写成绝对路径
+        swf: ctx + '/assets/common/webuploader-0.1.5/Uploader.swf',
+        // 文件接收服务端。此处根据自己的框架写，本人用的是SpringMVC
+        server: '/admin/webUploader',
+        // 选择文件的按钮。可选。
+        // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+        // pick: '#picker',
+        pick: {
+            id: '#picker',
+            label: '选择文件',
+            multiple:true  //默认为true，true表示可以多选文件，HTML5的属性
+        },
+        duplicate:true,//是否可重复选择同一文件
+        chunked: true,  //分片处理
+        chunkSize: 20 * 1024 * 1024, //每片20M
+        threads:3,//上传并发数。允许同时最大上传进程数。
+        prepareNextFile:true,// 在上传当前文件时，准备好下一个文件
+        // 禁掉全局的拖拽功能。
+        disableGlobalDnd: true,
+        accept: {
+            //限制上传文件为MP4
+            extensions: 'doc,docx，pdf',
+            mimeTypes: 'doc/docx/pdf'
+        }
+    });
+
+    // 当有文件添加进来的时候
+    uploader.on( 'fileQueued', function( file ) {
+        $list.append( '<div id="' + file.id + '" class="item">' +
+            '<h4 class="info">' + file.name + '</h4>' +
+            '<p class="state">等待上传...</p>' +
+            '</div>' );
+    });
+
+    // 文件上传过程中创建进度条实时显示。
+    uploader.on( 'uploadProgress', function( file, percentage ) {
+        var $li = $( '#'+file.id ),
+            $percent = $li.find('.progress .progress-bar');
+
+        // 避免重复创建
+        if ( !$percent.length ) {
+            $percent = $('<div class="progress progress-striped active">' +
+                '<div class="progress-bar" role="progressbar" style="width: 0%">' +
+                '</div>' +
+                '</div>').appendTo( $li ).find('.progress-bar');
+        }
+
+        $li.find('p.state').text('上传中');
+
+        $percent.css( 'width', percentage * 100 + '%' );
+    });
+
+    uploader.on( 'uploadSuccess', function( file ) {
+        $( '#'+file.id ).find('p.state').text('已上传');
+    });
+
+    uploader.on( 'uploadError', function( file ) {
+        $( '#'+file.id ).find('p.state').text('上传出错');
+    });
+
+    uploader.on( 'uploadComplete', function( file ) {
+        $( '#'+file.id ).find('.progress').fadeOut();
+    });
+
+    uploader.on( 'all', function( type ) {
+        if ( type === 'startUpload' ) {
+            state = 'uploading';
+        } else if ( type === 'stopUpload' ) {
+            state = 'paused';
+        } else if ( type === 'uploadFinished' ) {
+            state = 'done';
+        }
+
+        if ( state === 'uploading' ) {
+            $btn.text('暂停上传');
+        } else {
+            $btn.text('开始上传');
+        }
+    });
+
+    $btn.on( 'click', function() {
+        if ( state === 'uploading' ) {
+            uploader.stop();
+        } else {
+            uploader.upload();
+        }
+    });
+    /**
+     * 验证文件格式以及文件大小
+     */
+    uploader.on("error", function (type) {
+        debugger;
+        if (type == "Q_TYPE_DENIED") {
+            layer.msg("请上传doc,docx，pdf格式文件");
+        } else if (type == "Q_EXCEED_SIZE_LIMIT") {
+            layer.msg("文件大小不能超过3G");
+        }else {
+            layer.msg("上传出错！请检查后重新上传！错误代码"+type);
+        }
+
+    });
+});
