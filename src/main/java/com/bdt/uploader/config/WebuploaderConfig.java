68e3486abb4dcc25792ff27330857b3aa7dd4c7f
zhanghongbo <1002688187@qq.com>
2017-09-27 22:51:01 +0800

    添加变量配置

---
+++ src/main/java/com/bdt/uploader/config/WebuploaderConfig.java
@@ -0,0 +1,28 @@
+package com.bdt.uploader.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+/**
+ * Created by ZhangHongBo on 2017/9/25.
+ */
+@Service
+public class WebuploaderConfig {
+
+    @Value("${'dir.temlDir'}")
+    private static String tempDirectory;
+
+    /**
+     * 获取上传的临时目录
+     *
+     * @return
+     */
+    public static String getTempDirectory() {
+        if (tempDirectory != null && !tempDirectory.endsWith(File.separator)) {
+            tempDirectory += File.separator;
+        }
+        return tempDirectory;
+    }
+}
