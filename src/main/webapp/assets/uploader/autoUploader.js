71c3914d9ec9bc05c8b498e013c8e25dfb3de020
zhanghongbo001 <1002688187@qq.com>
2018-01-16 19:41:06 +0800

    增加公共插件：截取字符串

--- src/main/webapp/assets/uploader/autoUploader.js
+++ src/main/webapp/assets/uploader/autoUploader.js
@@ -14,11 +14,11 @@ $(function () {
             //时间点1：所有分块进行上传之前调用此函数
             beforeSendFile: function (file) {
                 var deferred = WebUploader.Base.Deferred();
-                uploader.md5File(file,0,9*1024*1024).progress(function (percentage) {
-                   $("#"+file.id).find('p.state').text('正在读取文件信息...');
+                uploader.md5File(file, 0, 9 * 1024 * 1024).progress(function (percentage) {
+                    $("#" + file.id).find('p.state').text('正在读取文件信息...');
                 }).then(function (val) {
-                    $("#"+file.id).find('p.state').text('正在读取文件信息...');
-                    file.fileMd5=val;
+                    $("#" + file.id).find('p.state').text('正在读取文件信息...');
+                    file.fileMd5 = val;
                     /*if(file.isUpload!==undefined && file.isUpload){
                         deferred.resolve();
                         return;
@@ -30,7 +30,7 @@ $(function () {
             //时间点2：如果有分块上传，则每个分块上传之前调用此函数
             beforeSend: function (block) {
                 var deferred = WebUploader.Base.Deferred();
-                block.file.chunks=block.chunks;
+                block.file.chunks = block.chunks;
                 $.ajax({
                     type: "POST",
                     url: "/uploaders/mergeOrCheckChunks?param=checkChunk",  //ajax验证每一个分片
@@ -52,7 +52,7 @@ $(function () {
                             deferred.resolve();
                         }
                     },
-                    error:function () {//任何形式的验证失败，都会触发重新上传
+                    error: function () {//任何形式的验证失败，都会触发重新上传
                         deferred.resolve();
                     }
                 });
@@ -60,7 +60,7 @@ $(function () {
             },
             //时间点3：所有分块上传成功后调用此函数
             afterSendFile: function (block) {
-                var beferred=WebUploader.Base.Deferred();
+                var beferred = WebUploader.Base.Deferred();
                 //如果分块上传成功，则通知后台合并分块
                 $.ajax({
                     type: "POST",
@@ -82,7 +82,7 @@ $(function () {
 
                         beferred.resolve();
                     },
-                    error:function () {
+                    error: function () {
                         beferred.resolve();
                     }
                 });
@@ -93,7 +93,7 @@ $(function () {
     /*************************** 初始化WebUploader start***************************/
     uploader = WebUploader.create({
         //选择文件，自动上传(自动上传设置成upload()方法上传，不然所有的拦截信息没用)
-        auto:false,
+        auto: false,
         // 不压缩image
         resize: false,
         // swf文件路径，需要用到flash的时候BASE_URL自己根据需要定义 也可写成绝对路径
@@ -102,7 +102,7 @@ $(function () {
         server: '/uploaders/webuploads',
         // 选择文件的按钮。可选。
         // 内部根据当前运行是创建，可能是input元素，也可能是flash.
-         pick: '#picker',
+        pick: '#picker',
         /*pick: {
             id: '#picker',
             label: '选择文件',
@@ -125,52 +125,53 @@ $(function () {
 
     // 当有文件添加进来的时候
     uploader.on('fileQueued', function (file) {
-        $("#upload").animate({marginBottom:"0px"})
-       $list.append(
-           '<div class="progr" id="'+file.id+'">'+
-           '<div class="al">'+
-           '<div style="width: 150px;" title="'+file.name+'" data-toggle="tooltip" data-placement="right">'+file.name+'</div>'+
-           '<div style="margin-left: 30px;"><p class="state" style="color: #080808;">等待上传...</p></div>'+
-           '</div>'+
-           '<div style="clear: both;"></div>'+
-           '<div class="progr del">'+
-           '<div class="progress">'+
-           '<div class="progress-bar" role="progressbar"'+
-           'aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">'+
-           '</div>'+
-           '</div>'+
-           '<div style="margin-left: 30px;"><a href="javascript:void(0);" class="file_btn btnRemoveFile"><i class="fa fa-times" aria-hidden="true" style="color: red;"></i></a></div>'+
-           '</div>'+
-           '<div style="clear: both;"></div>'+
-           '</div>'
-       );
-       uploader.upload(file.id);
+        $("#upload").animate({marginBottom: "0px"});
+        var fileNames = $.tooltipsExtracted(file.name,2,15);
+        $list.append(
+            '<div class="progr" id="' + file.id + '">' +
+            '<div class="al">' +
+            '<div style="width: 150px;" title="' + file.name + '" data-toggle="tooltip" data-placement="right">' + fileNames + '</div>' +
+            '<div style="margin-left: 30px;"><p class="state" style="color: #080808;">等待上传...</p></div>' +
+            '</div>' +
+            '<div style="clear: both;"></div>' +
+            '<div class="progr del">' +
+            '<div class="progress">' +
+            '<div class="progress-bar" role="progressbar"' +
+            'aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">' +
+            '</div>' +
+            '</div>' +
+            '<div style="margin-left: 30px;"><a href="javascript:void(0);" class="file_btn btnRemoveFile"><i class="fa fa-times" aria-hidden="true" style="color: red;"></i></a></div>' +
+            '</div>' +
+            '<div style="clear: both;"></div>' +
+            '</div>'
+        );
+        uploader.upload(file.id);
     });
 
     //当某个文件的分块在发送前触发，主要用来询问是否要添加附带参数，大文件在开起分片上传的前提下此事件可能会触发多次。
-    uploader.on('uploadBeforeSend',function (block,data,headers) {
-       data.fileMd5=block.file.fileMd5;
+    uploader.on('uploadBeforeSend', function (block, data, headers) {
+        data.fileMd5 = block.file.fileMd5;
     });
 
     //当所有文件上传结束时触发。
-    uploader.on('uploadFinished',function (block,data,headers) {
+    uploader.on('uploadFinished', function (block, data, headers) {
         setTimeout(function () {
-         $("#upload").animate({marginBottom:"-170px"})
-        },2000);
+            $("#upload").animate({marginBottom: "-170px"})
+        }, 2000);
     });
 
     //删除队列中的文件
-    $("body").delegate(".btnRemoveFile","click",function(){
+    $("body").delegate(".btnRemoveFile", "click", function () {
         var $this = $(event.target);
         var state = $this.parent().parent().parent().children().children().children();
-        if(state.text() == "文件已上传成功!"){
+        if (state.text() == "文件已上传成功!") {
             alert("文件已上传成功，不可以做删除操作！");
         }
-        var fileitem=$this.parent().parent().parent().parent();
-        var rmFileId= $(fileitem).attr("id");
+        var fileitem = $this.parent().parent().parent().parent();
+        var rmFileId = $(fileitem).attr("id");
         //删除内存文件
         uploader.removeFile(rmFileId, true);
-        $("#"+rmFileId,"#uploader").remove();
+        $("#" + rmFileId, "#uploader").remove();
     });
     // 文件上传过程中创建进度条实时显示。
     uploader.on('uploadProgress', function (file, percentage) {
@@ -204,16 +205,34 @@ $(function () {
     });
 
     $(".protop").click(function () {
-       showfind();
+        showfind();
     });
 });
 
+/***** 自定义插件 state******/
+$.extend({
+    //截取字符串
+    tooltipsExtracted: function (value, state, num) {
+        if (value != undefined) {
+            if (value.length > num) {
+                if (state == 1) {
+                    value = value.substring(0, num) + "...";
+                } else {
+                    value = value.substring(0, 10) + "...";
+                }
+            }
+        }
+        return value;
+    }
+});
+/***** 自定义插件 end******/
+
 //上传小窗 hide and show
 function showfind() {
-    var botton=$(".progressall").css('marginBottom');
-    if(botton=="0px"){
-        $("#upload").animate({marginBottom:"-170px"});
-    }else{
-        $("#upload").animate({marginBottom:"0px"});
+    var botton = $(".progressall").css('marginBottom');
+    if (botton == "0px") {
+        $("#upload").animate({marginBottom: "-170px"});
+    } else {
+        $("#upload").animate({marginBottom: "0px"});
     }
 }
